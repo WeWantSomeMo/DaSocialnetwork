d16628d12978e0e97399c6865a448ed52949ff65
WeWantSomeMo <mason.oz5@yahoo.com>
2022-10-26 18:58:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config();
+mongoose.connect(
+    process.env.MONGOurl),
+    {useNewUrlParser: true},
+    () => {
+    console.log('Connected to MongoDB')
+}
+
+
+
+
+
+
+app.listen(3002, ()=>{
+    console.log("Server is live!")
+})
