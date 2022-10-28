6ac764e22917643d896bf007c343662837579eb9
WeWantSomeMo <mason.oz5@yahoo.com>
2022-10-28 00:19:46 -0500

    trying to establish working routes without server crash

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+var router = require('./routes/users-route.js');
+// const {userRoute} = require("./routes/users-route.js")
 
 dotenv.config();
 
@@ -12,17 +14,31 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmed
   useUnifiedTopology: true,
 });
 
+mongoose.set('debug', true);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/",(req,res)=>{
-  res.send("Welcome to Da Social Network!")
+function userRoute(){
+app.use(require("./routes/users-route.js"))
+// app.use(app.router);
+// routes.initialize(app);
+};
+
+app.get("/", (req,res) =>{
+  res.send("Welcome to Homepage!")
 })
 
-app.get("/users",(req,res) => {
-  res.send("Welcome to User Netwok!")
-})
+app.get(userRoute(), (req,res) =>{
+  res.send("Welcome to User page!")
+}),
+
+// app.use("/api/users", router);
+
+
+module.exports = router 
 
 app.listen(3002, ()=>{
     console.log("Server is live!")
